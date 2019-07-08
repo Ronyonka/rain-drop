d62329dc29f2c4769efee39593df37cbc93f50a5
Ron Onyonka <rononyonka@gmail.com>
2019-07-08 10:02:26 +0300

    added tests

--- rain/tests.py
+++ rain/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase
+from datetime import date
+from .models import *
+from .forms import *
 
-# Create your tests here.
+
+class RainModelTest(TestCase):
+    def setUp(self):
+        self.rain = Rain.objects.create(date='2019-05-07', amount =900)
+
+    def testInstance(self):
+        self.assertTrue(isinstance(self.rain, Rain))
+        self.assertEqual(Rain.objects.count(), 1)
+
+
+    def test_form_data(self):
+        form_data = {'date':'2019-03-07', 'amount': 900}
+        form = RainForm(data=form_data)
+        self.assertTrue(form.is_valid)
+        form.save()
+        self.assertEqual(Rain.objects.count(), 2)
+
+    def test_cannot_post_future_date(self):
+        form_data = {'date':'2020-03-07', 'amount': 900}
+        form = RainForm(data=form_data)
+        self.assertTrue(form.is_valid)
+        form.save()
+        self.assertEqual(Rain.objects.count(), 2)
+
+    def test_cannot_post_duplicate_data(self):
+        form_data = {'date':'2019-03-07', 'amount': 900}
+        form = RainForm(data=form_data)
+        self.assertTrue(form.is_valid)
+        form.save()
+        self.assertEqual(Rain.objects.count(), 2)
+
+    def tearDown(self):
+        self.rain.delete()
--- rain/views.py
+++ rain/views.py
@@ -10,28 +10,28 @@ from .forms import *
 
 
 def home(request):
-   today = datetime.date.today()
-   startDate = request.GET.get('startDate', '2019-01-01')
-   endDate = request.GET.get('endDate', '2020-01-01')
-   if startDate > endDate:
-      startDate , endDate = '2019-01-01', '2020-01-01'
-   date_start = datetime.datetime.strptime(startDate, '%Y-%m-%d').date()
-   date_end = datetime.datetime.strptime(endDate, '%Y-%m-%d').date()
-   rain = Rain.objects.all()
-   rain = Rain.my_query.get_queryset().filter_by_date(date_start, date_end)
-   form = RainForm(request.POST)
-   if request.method == 'GET':
-      return render(request, 'main.html', {"rain":rain, "form":form})
-   if request.method == "POST":
-      if form.is_valid():
-         if  Rain.objects.filter(date = form.cleaned_data['date']).exists():
+    today = datetime.date.today()
+    startDate = request.GET.get('startDate', '2019-01-01')
+    endDate = request.GET.get('endDate', '2020-01-01')
+    if startDate > endDate:
+        startDate , endDate = '2019-01-01', '2020-01-01'
+    date_start = datetime.datetime.strptime(startDate, '%Y-%m-%d').date()
+    date_end = datetime.datetime.strptime(endDate, '%Y-%m-%d').date()
+    rain = Rain.objects.all()
+    rain = Rain.my_query.get_queryset().filter_by_date(date_start, date_end)
+    form = RainForm(request.POST)
+    if request.method == 'GET':
+        return render(request, 'main.html', {"rain":rain, "form":form})
+    if request.method == "POST":
+        if form.is_valid():
+            if  Rain.objects.filter(date = form.cleaned_data['date']).exists():
+                form = RainForm()
+            elif form.cleaned_data['date'] > today:
+                form = RainForm()
+            else:
+                form.save()
+                return redirect('home')
+        else:
             form = RainForm()
-         elif form.cleaned_data['date'] > today:
-            form = RainForm()
-         else:
-            form.save()
-         return redirect('home')
-      else:
-            form = RainForm()
-   return render(request, 'main.html', {'rain':rain, 'form': form})
+    return render(request, 'main.html', {'rain':rain, 'form': form})
 
