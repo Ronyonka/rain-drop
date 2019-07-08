76ed86aa85dc27890829f64044cf84e327b47449
Ron Onyonka <rononyonka@gmail.com>
2019-07-08 07:01:14 +0300

    added managers

--- rain/models.py
+++ rain/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from .managers import GeneralManager
+
 
 class Rain(models.Model):
     amount = models.IntegerField()
     date = models.DateField(auto_now=False)
+    objects = models.Manager()
+    my_query = GeneralManager()
 
 
     class Meta:
